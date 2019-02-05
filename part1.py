c4594eb32030ae5310f9e85d1421d27cf7ec8704
imerla1 <giorgi1048@yahoo.com>
2019-02-05 08:18:18 -0800

    text to speech

---
+++ part1.py
@@ -0,0 +1,9 @@
+import win32com.client
+
+# Calling the Disptach method of the module which  
+# interact with Microsoft Speech SDK to speak 
+# the given input from the keyboard 
+
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
+s = 'hello world'
+speaker.Speak(s)
